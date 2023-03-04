e663eed9a4067532a009cc52d0464ca5544223b1
Nicolas Lopez <nicolasmlopez00@gmail.com>
2023-03-04 20:19:48 -0300

    Probando estilos y contador

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+const [contador, setContador] = useState(0)
+
+const add = () =>{
+  setContador(contador + 1)
+}
+const subtract = () =>{
+  setContador(contador - 1)
+}
+const restart = () =>{
+  alert('Se reseteo el contador')
+  setContador(0)
+}
+
   return (
     <View style={styles.container}>
-      <Text>Bienvenido a CriptoCoin</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textStyle}>Bienvenido a CriptoCoin</Text>
+      <View style={styles.countContainer}>
+        <Button title ='Sumar' onPress={add} color = 'green'></Button>
+        <Text style={styles.textStyle}>{contador}</Text>
+        <Button title ='Restar' onPress={subtract} color = 'green'></Button>
+      </View>
+      <Button title ='Resetear' onPress={restart} color = 'red'></Button>
     </View>
   );
 }
@@ -13,8 +32,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontSize: 25,
+    fontStyle: 'italic',
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  countContainer:{
+    flexDirection: 'row-reverse'
+  }
+  
 });
