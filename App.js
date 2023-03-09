c45d70810d52edd5091f1e1161a7a4da477353ab
Nicolas Lopez <nicolasmlopez00@gmail.com>
2023-03-09 20:58:42 -0300

    Implemento de API coingecko

--- App.js
+++ App.js
@@ -1,33 +1,51 @@
-import { useState } from 'react';
-import { Text, View, TextInput, FlatList, TouchableOpacity, Alert } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Text, View, TextInput, FlatList, TouchableOpacity, Alert, ActivityIndicator } from 'react-native';
 import styles from './styles';
 export default function App() {
 
-const [cripto, setCripto] = useState([
-  {name:'Bitcoin', price:1000, key:'1'},
-  {name:'Ethereum', price:500, key:'2'},
-  {name:'Dogecoin', price:10, key:'3'},
-  {name:'Theter', price:1, key:'4'},
-  {name:'Swapcoin', price:50, key:'5'},
-])
+const [cripto, setCripto] = useState([])
+const [loading, setLoading] = useState(true)
+const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false"
 
-const showPrice = (item) =>{
-  console.log(item.name)
-  Alert.alert( item.name,`$${item.price} Usd`)
+const getData = async () => {
+  try{
+    const response = await fetch(url)
+    const json = await response.json()
+    setCripto(json)
+  } catch (error){
+    console.error(error)
+  } finally{
+    setLoading(false)
+  }
 }
+ useEffect(() => {
+  getData()
+}, [])
+
+const getPrice = (moneda) =>{
+  Alert.alert(moneda.name, `$${moneda.current_price} Usd`)
+}
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>CriptoCoin</Text>
       <TextInput style={styles.input} placeholder='Ingrese una criptomoneda' placeholderTextColor={'white'} onChangeText={(criptomoneda) => setCripto(criptomoneda)}/>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={cripto}
+          keyExtractor={({id}) => id}
+          renderItem={({item}) => (
+            <TouchableOpacity onPress={() => getPrice(item)}><Text style={styles.text}>
+              {item.name}, {item.symbol}
+            </Text></TouchableOpacity>
+          )}
+        />
+      )}
       
-      <FlatList
-        data = {cripto}
-        renderItem={({item})=>(
-          <View>
-            <TouchableOpacity onPress={() => showPrice(item)}><Text style={styles.text}>{item.name}</Text></TouchableOpacity>
-          </View>
-        )}
-      />     
+
+             
     </View>
   );
 }
